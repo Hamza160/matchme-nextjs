cd780f9b75d628b70fe911ba9cb3a4d29a1a043e
= <=>
2025-01-29 10:41:41 +0500

    Basic Setup + Routing + TopBar Design Done

---
+++ app/lists/page.tsx
@@ -0,0 +1,6 @@
+
+export default function ListsPage() {
+  return (
+    <div>ListsPage</div>
+  )
+}
---
+++ components/Providers.tsx
@@ -0,0 +1,12 @@
+import { HeroUIProvider } from "@heroui/react"
+import { PropsWithChildren } from "react"
+
+const Providers = ({children}:PropsWithChildren) => {
+  return (
+    <HeroUIProvider>
+        {children}
+    </HeroUIProvider>
+  );
+}
+
+export default Providers;
